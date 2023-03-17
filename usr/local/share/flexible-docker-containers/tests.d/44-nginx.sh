26691d3e48b4fc6645ea01fa048d18126d873518
Nigel Kukard <nkukard@lbsd.net>
2023-03-17 12:32:03 +0000

    feat: display log line if we don't run IPv6 tests

--- usr/local/share/flexible-docker-containers/tests.d/44-nginx.sh
+++ usr/local/share/flexible-docker-containers/tests.d/44-nginx.sh
@@ -37,6 +37,7 @@ fdc_test_pass nginx "Nginx is responding using IPv4"
 
 # Return if we don't have IPv6 support
 if [ -z "$(ip -6 route show default)" ]; then
+	fdc_test_alert nginx "Not running IPv6 tests due to no IPv6 default route"
 	return
 fi
 
